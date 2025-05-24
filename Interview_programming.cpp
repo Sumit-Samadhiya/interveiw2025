3a7caadd6061063754f10107e33c6492cb00634f
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2025-05-24 11:48:23 +0530

    1

---
+++ Interview_programming.cpp
@@ -0,0 +1,22 @@
+// Basic Programming Questions (Syntax & Logic)
+// 1. Write a program to print "Hello, World!".
+// 2. Take two numbers as input and print their sum.
+// 3. Check if a number is even or odd.
+// 4. Find the maximum of three numbers.
+// 5. Check if a year is a leap year.
+// 6. Print numbers from 1 to N using a loop.
+// 7. Find the sum of first N natural numbers.
+// 8. Print the multiplication table of a number.
+// 9. Calculate the factorial of a number (using loop).
+// 10. Calculate the power of a number without using built-in function.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // cout<<"Hello";
+
+    
+}
+

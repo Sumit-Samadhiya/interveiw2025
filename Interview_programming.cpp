eea9c81334f74245448d4504ca5270efc3bfe4a4
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2025-05-24 12:02:03 +0530

    7

--- Interview_programming.cpp
+++ Interview_programming.cpp
@@ -56,6 +56,14 @@ int main()
     //     cout<<i<<" ";
     // }
 
-    
+    // int n;
+    // cin>>n;
+    // int sum=0;
+    // for(int i=1;i<=n; i++)
+    // {
+    //   sum= sum+i;
+    // }
+    // cout<<sum;
+
 }
 

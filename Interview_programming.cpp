15f72329022b64620b01bc2bc3783c1996241bd1
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2025-05-25 10:31:54 +0530

    15

--- Interview_programming.cpp
+++ Interview_programming.cpp
@@ -151,7 +151,39 @@ int main()
     // else
     //     cout<<"not palindrome";
 
-    
+
+    #include <iostream>
+using namespace std;
+
+// Function to find GCD using Euclidean algorithm
+// int findGCD(int a, int b) {
+//     while (b != 0) {
+//         int temp = b;
+//         b = a % b;
+//         a = temp;
+//     }
+//     return a;
+// }
+
+// // Function to find LCM using GCD
+// int findLCM(int a, int b) {
+//     return (a * b) / findGCD(a, b);
+// }
+
+// int main() {
+//     int num1, num2;
+//     cout << "Enter two numbers: ";
+//     cin >> num1 >> num2;
+
+//     int gcd = findGCD(num1, num2);
+//     int lcm = findLCM(num1, num2);
+
+//     cout << "GCD of " << num1 << " and " << num2 << " is: " << gcd << endl;
+//     cout << "LCM of " << num1 << " and " << num2 << " is: " << lcm << endl;
+
+//     return 0;
+// }
+
 
 
 }
